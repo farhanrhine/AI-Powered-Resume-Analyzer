25a1059cfcc45d9d1111637b171c7b6cf1a18c59
Farhan <mohammadfarhanalam09@gmail.com>
2026-07-02 13:38:02 +0530

    feat: add resume_parser module to extract and clean text from PDF files

--- src/resume_parser.py
+++ src/resume_parser.py
@@ -1,1 +1,37 @@
 # extract text from resume and clean
+import pdfplumber
+from utils import clean_text
+
+
+def parser_resume(pdf_file) -> str:
+    """
+    Extract all text from pdf and return a clean string
+
+    args : a pdf file
+
+    return : a full resume text, cleaned.
+    """
+
+    all_text = []
+    
+    try:
+        with pdfplumber.open(pdf_file) as pdf:
+            for page in pdf.pages:
+                text = page.extract_text()
+
+                if text:
+                    all_text.append(text)
+    
+    except Exception as e:
+        print(f"Error reading PDF: {e}")
+        return ""
+
+    full_text = "\n".join(all_text)
+    return clean_text(full_text)
+
+
+
+if __name__ == "__main__":
+    with open("docs/FARHAN.pdf", "rb") as f:
+        result = parser_resume(f)
+        print(result)
